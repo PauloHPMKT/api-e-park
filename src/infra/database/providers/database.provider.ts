7bb1193b2ed6969fda2de0c78c9030a8ac2b909a
Paulo Sergio <pauloserg861@gmail.com>
2024-01-07 14:02:10 -0300

    feat: add method to get db_host env

--- src/infra/database/providers/database.provider.ts
+++ src/infra/database/providers/database.provider.ts
@@ -1,9 +1,13 @@
 import * as mongoose from 'mongoose';
+import { EnvConfigService } from 'src/infra/env-config/env-config.service';
 
 export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
-    useFactory: async (): Promise<typeof mongoose> =>
-      mongoose.connect('mongodb://db:27017/prodescriber'),
+    inject: [EnvConfigService],
+    useFactory: async (
+      envConfigService: EnvConfigService,
+    ): Promise<typeof mongoose> =>
+      mongoose.connect(envConfigService.getDbHost()),
   },
 ];
