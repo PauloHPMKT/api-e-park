d4d14a5d41918dc29f19b908e126e8e0b587a045
Paulo Sergio <pauloserg861@gmail.com>
2024-01-07 09:53:21 -0300

    feat: init OpenaiModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { OpenaiModule } from './modules/openai/openai.module';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [OpenaiModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
