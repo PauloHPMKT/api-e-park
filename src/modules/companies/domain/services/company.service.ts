0636be83a7314f886a9ca3e7d0ff89f1e15cc15f
Paulo Sergio <pauloserg861@gmail.com>
2024-01-07 14:06:12 -0300

    feat: add company service

---
+++ src/modules/companies/domain/services/company.service.ts
@@ -0,0 +1,21 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { CompanyEntity } from '../entities/Company';
+import { CreateCompanyDto } from '../../application/dto/create-company.dto';
+
+@Injectable()
+export class CompanyService {
+  constructor(
+    @Inject('COMPANY_MODEL')
+    private readonly companyModel: Model<CompanyEntity>,
+  ) {}
+
+  async findAll(): Promise<CompanyEntity[]> {
+    return await this.companyModel.find().exec();
+  }
+
+  async create(data: CreateCompanyDto): Promise<CompanyEntity> {
+    console.log(data);
+    return await this.companyModel.create(data);
+  }
+}
