143c40d31f1caef16aa8fd119f4c8e1f39790107
Paulo Sergio <pauloserg861@gmail.com>
2024-01-07 14:01:37 -0300

    refactor: export ConfigService

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { databaseProviders } from './providers/database.provider';
+import { EnvConfigModule } from '../env-config/env-config.module';
+import { EnvConfigService } from '../env-config/env-config.service';
 
 @Module({
-  providers: [...databaseProviders],
+  imports: [EnvConfigModule.forRoot()],
+  providers: [EnvConfigService, ...databaseProviders],
   exports: [...databaseProviders],
 })
 export class DatabaseModule {}
