f10b0345361fcec66b33cc19d71115557bb79090
Paulo Sergio <pauloserg861@gmail.com>
2024-01-07 14:06:28 -0300

    refactor: change origin folder

--- src/modules/companies/company.module.ts
+++ src/modules/companies/company.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { CompanyController } from './company.controller';
-import { CompanyService } from './company.service';
+import { CompanyController } from './application/company.controller';
+import { CompanyService } from './domain/services/company.service';
 import { DatabaseModule } from 'src/infra/database/database.module';
 import { companyProviders } from './infra/providers/company.providers';
 
