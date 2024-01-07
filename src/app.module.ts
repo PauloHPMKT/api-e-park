711bef97f81afd0d165b604f9c902b5b8941fd69
Paulo Sergio <pauloserg861@gmail.com>
2024-01-07 12:18:48 -0300

    feat: implement DatabaseModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,14 @@ import { OpenaiModule } from './modules/openai/openai.module';
 import { CompanyModule } from './modules/companies/company.module';
 import { EnvConfigModule } from './infra/env-config/env-config.module';
 import { EnvConfigService } from './infra/env-config/env-config.service';
+import { DatabaseModule } from './infra/database/database.module';
 @Module({
-  imports: [EnvConfigModule.forRoot(), CompanyModule, OpenaiModule],
+  imports: [
+    EnvConfigModule.forRoot(),
+    DatabaseModule,
+    CompanyModule,
+    OpenaiModule,
+  ],
   providers: [EnvConfigService],
   controllers: [],
 })
