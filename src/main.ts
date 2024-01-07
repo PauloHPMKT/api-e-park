e54837c70637aed68b40d61ca6be67d46d02b62d
Paulo Sergio <pauloserg861@gmail.com>
2024-01-07 10:20:44 -0300

    feat: add port config to init server

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+const logger = new Logger('main.ts');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = 3002;
+  await app
+    .listen(port)
+    .then(() =>
+      logger.log(`[DESCRIBER_API] is running on http://localhost:${port}`),
+    )
+    .catch((err) => logger.error(err));
 }
 bootstrap();
