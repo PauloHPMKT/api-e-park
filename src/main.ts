c780eee358e884326f81ebb9cf1bc3f474f47d24
Paulo Sergio <pauloserg861@gmail.com>
2024-01-07 10:24:49 -0300

    chore: install swagger ui for the application

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { AppModule } from './app.module';
 
 const logger = new Logger('main.ts');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('ProDescriber API')
+    .setDescription(
+      'ProDescriber API is an api for generating descriptions for products to bust sales.',
+    )
+    .setVersion('1.0')
+    .addTag('describer')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   const port = 3002;
   await app
     .listen(port)
