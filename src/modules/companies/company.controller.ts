1c91491ddc0f8c0d9b70b8dc4ec270f1869d19d0
Paulo Sergio <pauloserg861@gmail.com>
2024-01-07 12:15:55 -0300

    style: define structure

--- src/modules/companies/company.controller.ts
+++ src/modules/companies/company.controller.ts
@@ -1,8 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { CompanyService } from './company.service';
 
 @Controller('companies')
 export class CompanyController {
+  constructor(private readonly companyService: CompanyService) {}
+  @Get()
   getCompanies() {
-    return 'companies';
+    return this.companyService.findAll();
   }
 }
--- src/modules/companies/company.service.ts
+++ src/modules/companies/company.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class CompanyService {}
+export class CompanyService {
+  companies = [
+    {
+      id: 1,
+      name: 'Company 1',
+    },
+    {
+      id: 2,
+      name: 'Company 2',
+    },
+    {
+      id: 3,
+      name: 'Company 3',
+    },
+  ];
+  findAll(): { id: number; name: string }[] {
+    return this.companies;
+  }
+}
