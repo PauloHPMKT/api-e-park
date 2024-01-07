cf46f3bc3cf87759ec59dc647014fbb0c3ef3ad6
Paulo Sergio <pauloserg861@gmail.com>
2024-01-07 14:00:45 -0300

    feat: create method to get env from db_host

--- src/infra/env-config/env-config.service.ts
+++ src/infra/env-config/env-config.service.ts
@@ -12,4 +12,7 @@ export class EnvConfigService implements EnvConfig {
   getNodeEnv(): string {
     return this.configService.get<string>('NODE_ENV');
   }
+  getDbHost(): string {
+    return this.configService.get<string>('DB_HOST');
+  }
 }
