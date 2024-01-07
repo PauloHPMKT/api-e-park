b5e4e1bd7218a8f3350a1da6f6b0542113ec8888
Paulo Sergio <pauloserg861@gmail.com>
2024-01-07 12:14:31 -0300

    feat: create database module e provider

---
+++ src/infra/database/providers/database.provider.ts
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose';
+
+export const databaseProviders = [
+  {
+    provide: 'DATABASE_CONNECTION',
+    useFactory: async (): Promise<typeof mongoose> =>
+      mongoose.connect('mongodb://db:27017/prodescriber'),
+  },
+];
