1341f3c055449cdc06d4b0e17d97a180c944f129
Paulo Sergio <pauloserg861@gmail.com>
2024-01-09 10:10:41 -0300

    feat: add types to create usecases

--- src/modules/openai/infra/types/openai.interface.ts
+++ src/modules/openai/infra/types/openai.interface.ts
@@ -8,3 +8,18 @@ export interface ChatResponse {
   success: boolean;
   result: OpenAI.ChatCompletion.Choice;
 }
+
+export interface Message {
+  role: string;
+  content: string;
+}
+
+export interface OpenAIChat {
+  message: Message;
+  success: boolean;
+  result: {
+    message: Message;
+  };
+  created_at: Date;
+  updated_at: Date;
+}
