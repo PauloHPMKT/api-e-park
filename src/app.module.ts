a24dd632037bde7732239f6f129d9130e2330060
Paulo Sergio <pauloserg861@gmail.com>
2024-01-17 19:49:42 -0300

    feat: import modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,16 @@ import { OpenaiModule } from './modules/openai/openai.module';
 import { CompanyModule } from './modules/companies/company.module';
 import { EnvConfigModule } from './infra/env-config/env-config.module';
 import { EnvConfigService } from './infra/env-config/env-config.service';
+import { EmployeesModule } from './modules/employees/employees.module';
+import { AuthModule } from './modules/auth/auth.module';
 @Module({
-  imports: [EnvConfigModule.forRoot(), CompanyModule, OpenaiModule],
+  imports: [
+    EnvConfigModule.forRoot(),
+    AuthModule,
+    EmployeesModule,
+    CompanyModule,
+    OpenaiModule,
+  ],
   providers: [EnvConfigService],
   controllers: [],
 })
