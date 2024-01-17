7b81b975f6e6a13c88e9ee506d4c64f3f03d8d63
Paulo Sergio <pauloserg861@gmail.com>
2024-01-17 19:48:32 -0300

    feat: add validator to Company Entity

--- src/modules/companies/domain/entities/Company.ts
+++ src/modules/companies/domain/entities/Company.ts
@@ -7,12 +7,36 @@ export interface CompanyProps {
   phone: string;
   address: CompanyProps.Address;
   created_at?: Date;
-  updated_at?: Date;
 }
 
 export class CompanyEntity {
   constructor(private readonly props: CompanyProps) {
     this.props.created_at = this.props.created_at ?? new Date();
-    this.props.updated_at = this.props.updated_at ?? new Date();
+    this.validate(props);
+  }
+
+  private validate(props: CompanyProps) {
+    const requiredFields = Object.keys(props);
+    for (const field of requiredFields) {
+      if (!props[field]) {
+        throw new Error(`The field ${field} is required`);
+      }
+    }
   }
 }
+
+console.log(
+  new CompanyEntity({
+    name: 'string',
+    cpnj: 'string/0001-01',
+    email: 'string@mail.com',
+    phone: '85988704658',
+    address: {
+      street: 'rua string',
+      city: 'cidade string',
+      state: 'estado string',
+      country: 'pais string',
+      zipCode: '00000-000',
+    },
+  }),
+);
