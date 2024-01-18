3850e13a2ac52fba3ef1c639d5f5623fd0b5311c
Paulo Sergio <pauloserg861@gmail.com>
2024-01-18 19:39:29 -0300

    refactor: apply process.env to prevent strange behavior

--- src/modules/auth/infra/providers/jwt-register.provider.ts
+++ src/modules/auth/infra/providers/jwt-register.provider.ts
@@ -4,7 +4,7 @@ import { EnvConfigService } from 'src/infra/env-config/env-config.service';
 export const jwtRegisterProvider = {
   imports: [EnvConfigModule],
   useFactory: async (envConfigService: EnvConfigService) => ({
-    secret: envConfigService.getSecretKey(),
+    secret: process.env.JWT_SECRET || envConfigService.getSecretKey(),
     signOptions: { expiresIn: '24h' },
   }),
   inject: [EnvConfigService],
