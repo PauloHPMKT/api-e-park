fef6f14a0ea90fd7607c38200f2047c4769ee7b9
Paulo Sergio <pauloserg861@gmail.com>
2024-01-18 15:44:38 -0300

    feat: add modules to create types

---
+++ src/infra/models/payload-props.ts
@@ -0,0 +1,10 @@
+export interface PayloadProps {
+  sub?: string;
+  _id?: string;
+  name?: string;
+  email?: string;
+  role?: string;
+  role_gpt_generate?: string;
+  iat?: number;
+  exp?: number;
+}
