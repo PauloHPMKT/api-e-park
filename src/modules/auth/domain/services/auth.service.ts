ef7c9ad9e0bf86bfaa50c68a0911408130839281
Paulo Sergio <pauloserg861@gmail.com>
2024-01-18 15:43:43 -0300

    feat: implement auth service login

--- src/modules/auth/domain/services/auth.service.ts
+++ src/modules/auth/domain/services/auth.service.ts
@@ -1,13 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import { EmployeesService } from 'src/modules/employees/domain/services/employees.service';
 import { Encript } from 'src/infra/encript';
+import { PayloadProps } from 'src/infra/models/payload-props';
+import { JwtService } from '@nestjs/jwt';
+import { Token } from 'src/infra/models/token';
+import { CreateEmployeeDto } from 'src/modules/employees/application/dto/CreateEmployees.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly employeeService: EmployeesService) {}
+  constructor(
+    private readonly employeeService: EmployeesService,
+    private readonly jwtService: JwtService,
+  ) {}
 
-  async login() {
-    return 'feito login';
+  login(user: CreateEmployeeDto): Token {
+    const payload: PayloadProps = {
+      sub: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      role_gpt_generate: user.role_gpt_generate,
+    };
+
+    const jwtToken = this.jwtService.sign(payload);
+    return {
+      access_token: jwtToken,
+      user,
+    };
   }
 
   async validateUser(email: string, password: string) {
