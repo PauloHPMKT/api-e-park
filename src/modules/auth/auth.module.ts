b9140d7313319bc49cd6977cc174726b337d1f4a
Paulo Sergio <pauloserg861@gmail.com>
2024-01-18 19:40:05 -0300

    fix: resolve wrong caller for jwtProvider

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { DatabaseModule } from 'src/infra/database/database.module';
 import { AuthController } from './application/auth.controller';
 import { AuthService } from './domain/services/auth.service';
@@ -6,7 +7,6 @@ import { EmployeesModule } from '../employees/employees.module';
 import { EnvConfigModule } from 'src/infra/env-config/env-config.module';
 import { EnvConfigService } from 'src/infra/env-config/env-config.service';
 import { LocalStrategy } from './application/guards/strategies/local.strategy';
-import { JwtModule, JwtModuleOptions } from '@nestjs/jwt';
 import { JwtStrategy } from './application/guards/strategies/jwt.strategy';
 import { jwtRegisterProvider } from './infra/providers/jwt-register.provider';
 
@@ -15,7 +15,7 @@ import { jwtRegisterProvider } from './infra/providers/jwt-register.provider';
     EnvConfigModule.forRoot(),
     DatabaseModule,
     EmployeesModule,
-    JwtModule.register(jwtRegisterProvider as JwtModuleOptions),
+    JwtModule.registerAsync(jwtRegisterProvider),
   ],
   controllers: [AuthController],
   providers: [AuthService, EnvConfigService, JwtStrategy, LocalStrategy],
