59dfd163b1371e9dc5d41d8f940c5d537f21fd9a
Paulo Sergio <pauloserg861@gmail.com>
2024-01-23 19:05:55 -0300

    feat: implement middleware validate

--- src/modules/openai/openai.module.ts
+++ src/modules/openai/openai.module.ts
@@ -1,14 +1,20 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { OpenaiController } from './application/openai.controller';
 import { OpenaiService } from './domain/services/openai.service';
 import { EnvConfigModule } from 'src/infra/env-config/env-config.module';
 import { EnvConfigService } from 'src/infra/env-config/env-config.service';
 import { openaiProviders } from './infra/providers/openai.providers';
 import { DatabaseModule } from 'src/infra/database/database.module';
+import { AuthOpenaiMiddleware } from './application/middlewares/auth-openai.middleware';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule, EnvConfigModule.forRoot()],
+  imports: [AuthModule, DatabaseModule, EnvConfigModule.forRoot()],
   controllers: [OpenaiController],
   providers: [EnvConfigService, OpenaiService, ...openaiProviders],
 })
-export class OpenaiModule {}
+export class OpenaiModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(AuthOpenaiMiddleware).forRoutes('openai/chat');
+  }
+}
