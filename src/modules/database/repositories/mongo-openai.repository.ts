1d3fd068cac0839cdfc5470e540a38fdc5b96234
Paulo Sergio <pauloserg861@gmail.com>
2024-01-23 20:59:32 -0300

    feat: create MongoOpenaiRepository

---
+++ src/modules/database/repositories/mongo-openai.repository.ts
@@ -0,0 +1,15 @@
+import { Inject } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { OpenaiRepository } from 'src/modules/openai/infra/repositories/openai.repository';
+import { OpenAIChat } from 'src/modules/openai/infra/types/openai.interface';
+
+export class MongoOpenaiRepository implements OpenaiRepository {
+  constructor(
+    @Inject('OPENAI_MODEL') private readonly openaiModel: Model<OpenAIChat>,
+  ) {}
+
+  async save(content: OpenAIChat): Promise<OpenAIChat> {
+    const response = await this.openaiModel.create(content);
+    return response;
+  }
+}
