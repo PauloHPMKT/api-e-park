fd543f90a203ec57320c88f96bb86de540008403
Paulo Sergio <pauloserg861@gmail.com>
2024-01-23 21:01:07 -0300

    refactor: implement repository provider

--- src/modules/openai/openai.module.ts
+++ src/modules/openai/openai.module.ts
@@ -3,15 +3,21 @@ import { OpenaiController } from './application/openai.controller';
 import { OpenaiService } from './domain/services/openai.service';
 import { EnvConfigModule } from 'src/infra/env-config/env-config.module';
 import { EnvConfigService } from 'src/infra/env-config/env-config.service';
-import { openaiProviders } from './infra/providers/openai.providers';
 import { DatabaseModule } from '../database/database.module';
 import { AuthOpenaiMiddleware } from './application/middlewares/auth-openai.middleware';
 import { AuthModule } from '../auth/auth.module';
+import { openaiDbProviders } from './infra/providers/openai-database.providers';
+import { openaiProviders } from './infra/providers/openai.providers';
 
 @Module({
   imports: [AuthModule, DatabaseModule, EnvConfigModule.forRoot()],
   controllers: [OpenaiController],
-  providers: [EnvConfigService, OpenaiService, ...openaiProviders],
+  providers: [
+    EnvConfigService,
+    OpenaiService,
+    ...openaiDbProviders,
+    ...openaiProviders,
+  ],
 })
 export class OpenaiModule implements NestModule {
   configure(consumer: MiddlewareConsumer): void {
