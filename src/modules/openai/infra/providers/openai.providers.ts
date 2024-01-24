9d81a7f6cf3bc773eb49944285282e22d191797b
Paulo Sergio <pauloserg861@gmail.com>
2024-01-23 21:03:16 -0300

    feat: create provide to instance repository

--- src/modules/openai/infra/providers/openai.providers.ts
+++ src/modules/openai/infra/providers/openai.providers.ts
@@ -1,11 +1,8 @@
-import { Connection } from 'mongoose';
-import { OpenaiSchema } from '../schema/openai.schema';
+import { MongoOpenaiRepository } from 'src/modules/database/repositories/mongo-openai.repository';
 
 export const openaiProviders = [
   {
-    provide: 'OPENAI_MODEL',
-    useFactory: (connection: Connection) =>
-      connection.model('OpenaiChat', OpenaiSchema),
-    inject: ['DATABASE_CONNECTION'],
+    provide: 'OpenaiRepository',
+    useClass: MongoOpenaiRepository,
   },
 ];
